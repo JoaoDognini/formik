26bcfaa1b7ac8c9f052fc505670c764bb60c0cfe
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2024-06-10 09:40:17 -0300

    Aula 3 - Validando dados
    Adicionado validações para os campos e mostrados na tela ao enviar formulário

--- src/paginas/cadastro/DadosPessoais.jsx
+++ src/paginas/cadastro/DadosPessoais.jsx
@@ -51,16 +51,40 @@ const DadosPessoais = () => {
 
             validate={(values) => {
                 const errors = {};
-                if (!values.nome) errors.nome = 'Campo obrigatório';
-
+                if (!values.nome) {
+                    errors.nome = 'Campo obrigatório'
+                }
+                if (!values.estado) {
+                    errors.estado = 'Campo obrigatório';
+                }
+                if (!values.cidade) {
+                    errors.cidade = 'Campo obrigatório';
+                }
+                if (!values.senha) {
+                    errors.senha = 'Campo obrigatório';
+                }
+                if (!values.telefone) {
+                    errors.telefone = 'Campo obrigatório'
+                } else if (!/^\d{11}$/i.test(values.telefone)) {
+                    errors.telefone = 'Número de telefone inválido'
+                }
+                if (!values.email) {
+                    errors.email = 'Campo obrigatório'
+                } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                    errors.email = 'Email inválido'
+                }
                 if (!values.confirmarSenha) {
                     errors.confirmarSenha = 'Campo obrigatório'
                 } else if (values.senha != values.confirmarSenha) {
-                    errors.confirmarSenha = 'As senhas não são iguais.'
+                    errors.confirmarSenha = 'As senhas não conferem'
                 }
+                return errors;
+            }}
+            onSubmit={(values) => {
+                console.log('dados do formulário', values)
             }}
         >
-            {formik => {
+            {formik => (
                 <Form onSubmit={formik.handleSubmit}>
                     <div style={{ textAlign: 'center' }}>
                         <Tipografia variante="h1" componente="h1">
@@ -146,7 +170,7 @@ const DadosPessoais = () => {
                         </Col>
                     </Row>
                 </Form>
-            }}
+            )}
         </Formik>
     );
 };
